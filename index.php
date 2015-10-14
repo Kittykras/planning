85a22b55796e5c7f19208652b1267289be463866
Kittykras <Silqe@hotmail.com>
2015-10-14 08:41:24 +0200

    format

--- index.php
+++ index.php
@@ -4,20 +4,20 @@ include 'include/top.inc.php';
 <link rel="stylesheet" href="login.css">
 <div class="vertically-align" align="center">
     <form role="form" action="checkLogin.php" method="post">
-    <div class="form-group">
-        <input name="user" type="user" class="form-control" id="user" placeholder="Brugernavn" autocomplete="on">
-    </div>
-    <div class="form-group">
-        <input name="pwd" type="password" class="form-control" id="pwd" placeholder="Kodeord" autocomplete="on">
-    </div>
-    <button type="submit" class="btn btn-black">Log Ind</button>
-  </form>
+        <div class="form-group">
+            <input name="user" type="user" class="form-control" id="user" placeholder="Brugernavn" autocomplete="on">
+        </div>
+        <div class="form-group">
+            <input name="pwd" type="password" class="form-control" id="pwd" placeholder="Kodeord" autocomplete="on">
+        </div>
+        <button type="submit" class="btn btn-black">Log Ind</button>
+    </form>
 </div>
 
 </body>
 </html>
 <?php
-if(isset($_GET["error"])){
+if (isset($_GET["error"])) {
     echo 'Wrong username or password';
 }
 ?>
