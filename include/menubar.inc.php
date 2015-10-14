e344300d8e7042ceb50efd023b52d2abc049196f
Kittykras <Silqe@hotmail.com>
2015-10-14 08:32:40 +0200

    menubar include added

--- include/menubar.inc.php
+++ include/menubar.inc.php
@@ -1,8 +1,27 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div id='cssmenu' style="max-width: 940px; margin: auto">
+            <ul>
+<!--                <li><a href='#'><span style="color: orangered">+</span>vonbülow.co</a></li>-->
+                <div class="navbar-header">
+                    <a class="navbar-brand" style="color: white" href="#"><span style="color: orangered">+</span>vonbülow.co</a>
+                </div>
+                <li class='has-sub'><a href='#'>Medarbejdere</a>
+                    <ul>
+                        <li class='has-sub'><a href='#'>Rikke</a>
+                        </li>
+                        <li class='has-sub'><a href='#'>Ane</a>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href='#'>Kunder</a>
+                    <ul>
+                        <li class='has-sub'><a href='#'>SFB</a>
+                        </li>
+                        <li class='has-sub'><a href='#'>Ekko</a>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href='#'>Overblik</a></li>
+                <li><a href='#'>Time Oversigt</a></li>
+                <li><a href='#'><span class="glyphicon glyphicon-user"></span> Rikke</a></li>
+            </ul>
+        </div>
