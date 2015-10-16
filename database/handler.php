28ec100dfbd70fbf710df3d60407b88d66d73115
Kittykras <Silqe@hotmail.com>
2015-10-16 09:27:38 +0200

    trying to make function work

--- database/handler.php
+++ database/handler.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'database/DBConnection.php';
+
+require_once 'DBConnection.php';
 require_once 'classes/User.php';
 
 $db = new DBConnection();
@@ -8,5 +9,14 @@ $stmt = $db->prepare($q);
 $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
 $stmt->execute();
 $users = $stmt->fetchAll();
-//print_r($users);
 
+//print_r($users);
+function getUserFromCookie() {
+    $db = new DBConnection();
+    $q = "call getassociate(" + $_COOKIE["UserName"] + ")";
+    $stmt = $db->prepare($q);
+    $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+    $stmt->execute();
+    $user = $stmt->fetchObj();
+    return $user;
+}
--- include/top.inc.php
+++ include/top.inc.php
@@ -8,7 +8,6 @@
         <link rel="stylesheet" href="html5reset.css">
         <!--Latest compiled and minified CSS--> 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
         <!--Optional theme--> 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
         <!--Latest compiled and minified JavaScript--> 
