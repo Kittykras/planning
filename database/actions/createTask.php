9d86ccd894028b34f18a340cb8871bee1b1f2f53
Kittykras <Silqe@hotmail.com>
2015-10-28 11:57:07 +0100

    createtask fixed??

--- database/actions/createTask.php
+++ database/actions/createTask.php
@@ -1,14 +1,16 @@
 <?php
+
 require_once '../DBConnection.php';
 session_start();
-try{
-    $user = $_SESSION["user"]->a_username;;
+try {
+    $user = $_SESSION["user"]->a_username;
+    ;
     $cus = $_COOKIE["Kunde"];
     $title = $_POST["title"];
     $descr = $_POST["descr"];
     $stat = $_POST["stat"];
     $assi = $_POST["assi"];
-    $timespen = $_POST["hour"].":".$_POST["min"];
+    $timespen = $_POST["hour"] . ":" . $_POST["min"];
     $from = $_POST["from"];
     $to = $_POST["to"];
     $comment = $_POST["newComment"];
@@ -19,10 +21,12 @@ try{
     $stmt = $db->prepare($q);
     $stmt->execute(array(':user' => $user, ':cus' => $cus, ':title' => $title, ':descr' => $descr, ':stat' => $stat, ':assi' => $assi, ':timespent' => $timespen, ':from' => $from, ':to' => $to, ':inv' => $inv, ':exp' => $exp));
     $count = $stmt->rowCount();
-    $q = "call createcommentonnewtask(:comment, :user);";
-    $stmt = $db->prepare($q);
-    $stmt->execute(array(':comment' => $comment, ":user" => $user));
-    if($count == 1){
+    if ($comment != "") {
+        $q = "call createcommentonnewtask(:comment, :user);";
+        $stmt = $db->prepare($q);
+        $stmt->execute(array(':comment' => $comment, ":user" => $user));
+    }
+    if ($count == 2) {
         header("location:../../enkeltKunde.php");
     } else {
         header("location:../../opretOpgave.php?error");
