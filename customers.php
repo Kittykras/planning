e332247e9ecbf55e0fd2e67c9fdaa75023199852
Kittykras <Silqe@hotmail.com>
2015-11-04 13:08:07 +0100

    removed outcommented code and replaced titel from vonbulowplanning to planning

--- customers.php
+++ customers.php
@@ -56,16 +56,6 @@ include './include/menubar.inc.php';
             $("div#new").removeClass("hidden");
         }
     });
-//    function SetCookie(c_name, value, expiredays) {
-//        var exdate = new Date()
-//        exdate.setDate(exdate.getDate() + expiredays)
-//        document.cookie = c_name + "=" + escape(value) +
-//                ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString()+";path=/vonbulowPlanning/")
-//    }
-//    function redirect(user) {
-//        document.cookie = "Kunde=" + user;
-//        window.location = 'enkeltKunde.php';
-//    }
 </script>
 </body>
 </html>
