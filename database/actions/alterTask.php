276b3e6b0ce0916ac08df182d07b6e7eece28e80
Kittykras <Silqe@hotmail.com>
2015-11-25 11:11:01 +0100

    fixed active cookies from task

--- database/actions/alterTask.php
+++ database/actions/alterTask.php
@@ -50,7 +50,25 @@ try {
     }
     if ($stmt != FALSE) {
         setcookie("Kunde", $cus, time() + (86400), "/planning/");
-        header("location:" . $_COOKIE['previous']);
+        $previous = $_COOKIE['previous'];
+        if (strpos($previous, 'associate') != FALSE) {
+            setcookie('kunder', '', time() + (86400), "/planning/");
+            if ($_COOKIE['UserName'] === $_SESSION["user"]->a_username) {
+                setcookie('login', 'active', time() + (86400), "/planning/");
+            } else {
+                setcookie('medarbejder', 'active', time() + (86400), "/planning/");
+            }
+        } else if (strpos($previous, 'time') != FALSE) {
+            setcookie('kunder', '', time() + (86400), "/planning/");
+            setcookie('timeoversigt', 'active', time() + (86400), "/planning/");
+        } else if (strpos($previous, 'overview') != FALSE) {
+            setcookie('kunder', '', time() + (86400), "/planning/");
+            setcookie('overblik', 'active', time() + (86400), "/planning/");
+        } else if (strpos($previous, 'press') != FALSE) {
+            setcookie('kunder', '', time() + (86400), "/planning/");
+            setcookie('presse', 'active', time() + (86400), "/planning/");
+        }
+        header("location:" . $previous);
     } else {
         header("location:../../taskForm.php?edit&error");
     }
--- database/actions/deleteTask.php
+++ database/actions/deleteTask.php
@@ -1,15 +1,34 @@
 <?php
+
 require_once '../DBConnection.php';
 
-try{
+try {
     $delName = $_COOKIE["Task"];
     $db = new DBConnection();
     $q = "call deletetask(:delName);";
     $stmt = $db->prepare($q);
-    $stmt->execute(array(":delName"=>$delName));
+    $stmt->execute(array(":delName" => $delName));
     $count = $stmt->rowCount();
-    if($stmt != FALSE){
-        header("location:".$_COOKIE['previous']);
+    if ($stmt != FALSE) {
+        $previous = $_COOKIE['previous'];
+        if (strpos($previous, 'associate') != FALSE) {
+            setcookie('kunder', '', time() + (86400), "/planning/");
+            if ($_COOKIE['UserName'] === $_SESSION["user"]->a_username) {
+                setcookie('login', 'active', time() + (86400), "/planning/");
+            } else {
+                setcookie('medarbejder', 'active', time() + (86400), "/planning/");
+            }
+        } else if (strpos($previous, 'time') != FALSE) {
+            setcookie('kunder', '', time() + (86400), "/planning/");
+            setcookie('timeoversigt', 'active', time() + (86400), "/planning/");
+        } else if (strpos($previous, 'overview') != FALSE) {
+            setcookie('kunder', '', time() + (86400), "/planning/");
+            setcookie('overblik', 'active', time() + (86400), "/planning/");
+        } else if (strpos($previous, 'press') != FALSE) {
+            setcookie('kunder', '', time() + (86400), "/planning/");
+            setcookie('presse', 'active', time() + (86400), "/planning/");
+        }
+        header("location:" . $previous);
     } else {
         header("location:../../taskForm.php?edit&error");
     }
